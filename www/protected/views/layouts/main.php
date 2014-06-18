8787ad553a0715cbc8809a227bf0d281696e46f0
Joko Putranto <akun.joko.putranto@gmail.com>
2014-06-18 12:03:44 +0700

    hapus about & kontak

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -28,8 +28,6 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about'),'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Contact', 'url'=>array('/site/contact'),'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Perekaman', 'url'=>array('/kategori/index'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
